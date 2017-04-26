55de15275caaa53bedd23ff2271ca08a9aa2dc7d
Alberto <alber7rp@gmail.com>
2017-04-26 09:59:17 +0100

    triangulo desarrollado

---
+++ index.js
@@ -0,0 +1,12 @@
+let Shape = require('@alber7rp/ull-shape-alu0100825510');
+
+class Triangle extends Shape {
+    constructor(options) {
+      super(options)
+    }
+    area() {
+      return 0.5 * this.width * this.height;
+    }
+}
+Shape.Shapes.Triangle = Triangle;
+module.exports = Triangle;
